65a259d3568023bb6ca6c57cfc18681ae2af8d1d
rebo95 <arebored@ucm.es>
2020-02-16 11:25:56 +0100

    starting with the interaction system

--- Unity Prototype/Assets/Scripts/CharacterInteraction.cs
+++ Unity Prototype/Assets/Scripts/CharacterInteraction.cs
@@ -5,23 +5,52 @@ using UnityEngine.UI;
 
 public class CharacterInteraction : MonoBehaviour
 {
+
+
+
     [SerializeField]
     Text dialogeText;
+
+    [SerializeField]
+    Text questionOption1Text;
+    [SerializeField]
+    Text questionOption2Text;
+
+    [SerializeField]
+    string questionOption1;
+    [SerializeField]
+    string questionOption2;
+
+    [SerializeField]
+    Button questionOption1Button;
+    [SerializeField]
+    Button questionOption2Button;
 
     [SerializeField]
     GameObject panel;
 
 
     private int numOfDialogues;
+    private int numExtraDialogues;
 
     [SerializeField]
     List<string> dialoges;
+
+    [SerializeField]
+    int questionIndex;
+
+
+    private bool questCompleted = false;
 
     private int interactionIndex = -1;
     // Start is called before the first frame update
     void Start()
     {
-        numOfDialogues = dialoges.Count;
+        numOfDialogues = dialoges.Count + 1;// Debemos tener en cuenta la pregunta
+        dialoges.Add("");
+        questionOption1Text.text = questionOption1;
+        questionOption2Text.text = questionOption2;
+
     }
 
     // Update is called once per frame
@@ -45,8 +74,26 @@ public class CharacterInteraction : MonoBehaviour
         if (interactionIndex == -1)
         {
             panel.SetActive(false);
+            RemoveExtraLines();
+
+            if (questCompleted)
+            {
+                gameObject.SetActive(false);
+            }
+
         }
-        else panel.SetActive(true);
+        else if (interactionIndex == 0) {
+            panel.SetActive(true);
+        }
+        else if (interactionIndex == questionIndex)
+        {
+            questionOption1Button.gameObject.SetActive(true);
+            questionOption2Button.gameObject.SetActive(true);
+        }else if (interactionIndex == questionIndex + 1)
+        {
+            questionOption1Button.gameObject.SetActive(false);
+            questionOption2Button.gameObject.SetActive(false);
+        }
     }
 
     private void UpdateDialogue()
@@ -56,4 +103,40 @@ public class CharacterInteraction : MonoBehaviour
         dialogeText.text = displayedText;
     }
     
+
+    public void Button1Action()
+    {
+
+        questCompleted = true;
+        AddDialogeLine("Correcto");
+        AddDialogeLine("Pues...");
+        AddDialogeLine("Yo ya me voy");
+        AddDialogeLine("...");
+
+        Interact();
+    }
+
+    public void Button2Action()
+    {
+        AddDialogeLine("HAHAHAHAHAAH");
+        AddDialogeLine("NO");
+
+
+        Interact();
+
+    }
+
+    private void AddDialogeLine(string line) {
+        dialoges.Add(line);
+        numOfDialogues++;
+        numExtraDialogues++;
+    }
+
+    private void RemoveExtraLines() {
+
+        int firstExtraLineIndex = numOfDialogues - numExtraDialogues;
+        dialoges.RemoveRange(firstExtraLineIndex, numExtraDialogues);
+        numOfDialogues -= numExtraDialogues;
+        numExtraDialogues = 0;
+    }
 }
